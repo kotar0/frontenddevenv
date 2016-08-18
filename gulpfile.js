bfc3f0244b5bb398dc39cb8c4f3a371efbe15b44
kb_okuya <okuya-kotaro@karabiner-tech.jp>
2016-08-18 16:55:50 +0900

    更新 Gulpfilenの編集

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var sync = require('browser-sync');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var cssnext = require('postcss-cssnext');
+var sourcemaps = require('gulp-sourcemaps');
+var processors = [cssnext()]; //cssnextのオプション等指定。
+
+
+
+
+gulp.task('sass', function () {
+  return gulp.src('dev/sass/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'expanded',
+      sourcemap: true
+    }))
+    .on('error', function (err) {
+      console.log(err.message);
+    })
+    .pipe(postcss(processors)) //prefixなどを追加
+    .pipe(sourcemaps.write('maps/'))
+    .pipe(gulp.dest('docs/css/'))
+    .pipe(sync.stream()) //これを足すとライブリロードに対応。
+})
+
+
+//publish サーバーアップ用(ソースマップや必要なJSのみにしている)
+gulp.task('publish', function () {
+  gulp.src('dev/sass/*.scss')
+    .pipe(sass())
+    .on('error', function (err) {
+      console.log(err.message);
+    })
+    .pipe(postcss(processors)) //prefixなどを追加
+    .pipe(gulp.dest('publish/css/'))
+})
+
+//Browsersync
+gulp.task('server', function () {
+  sync.init({
+    server: {
+      baseDir: './docs'
+    }
+  });
+})
+
+//Watchタスクを別のタスクと分けて書いている
+//Write dowm watch task apart from others.
+gulp.task('watch', function () {
+  gulp.watch('dev/sass/*.scss', ['sass']);
+})
+
+gulp.task('default', ['server', 'watch'], function () {
+
+})
+
+
+
+/*
+dev以下と、src/image以下の中が更新されたら、自動リロード
+
+
+*/
